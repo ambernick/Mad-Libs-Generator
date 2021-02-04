18549e0f476782d6205f1bab36b4dc7868fc159f
ambernick <ambernich2@yahoo.com>
2021-02-04 02:37:05 -0500

    add test function/ sanity check for word input in description

--- MadLibs/main.py
+++ MadLibs/main.py
@@ -1,7 +1,16 @@
 #Mad Libs Random Story Generator
 
+class MadLibs:
+    def _init_(self, word_descriptions, template):
+        self.template = templateself.word_descriptions = word_descriptions 
 #Template
 
 #User Input
-
+word_descriptions = ["noun", "verb"]
+words = []
+print("Please provide the following words: ")
+for desc in word_descriptions:
+    user_input = input(desc + " ")
+    words.append(user_input)
+print(words)
 #Story Building
