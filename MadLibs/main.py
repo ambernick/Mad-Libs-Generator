707e6260aaf8664fc262be638f8c341d1bdabd81
ambernick <ambernich2@yahoo.com>
2021-02-07 03:29:00 -0500

    render to home page, add new files, add browser functionality, map home page

--- MadLibs/main.py
+++ MadLibs/main.py
@@ -16,20 +16,20 @@ def get_words_from_user(word_descriptions):
     return words
 
 #Story Building
-def build_story(template, words):
-    story = template.format(*words)
-    return story
+# def build_story(template, words):
+#     story = template.format(*words)
+#     return story
 
-def get_template(name, path="./templates"):
-    fpath = os.path.join(path, name)
-    print(fpath)
-    print(os.path.exists(fpath))
+# def get_template(name, path="./templates"):
+#     fpath = os.path.join(path, name)
+#     print(fpath)
+#     print(os.path.exists(fpath))
     # with open(fpath, "r") as f:
     #     data = json.load(f)
     # print(data)
 
-temp_name = "vacations.json"
-get_template(temp_name)
+# temp_name = "vacations.json"
+# get_template(temp_name)
 
 # template = "I own a big {}. I like to {}."
 # words = get_words_from_user(["noun", "verb"])
