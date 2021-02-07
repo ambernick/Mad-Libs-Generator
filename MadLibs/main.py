f04438d7925822ce39f4fff8fb89fff1dc0a211f
ambernick <ambernich2@yahoo.com>
2021-02-07 04:19:02 -0500

    start index functions and function stub ups on view page

--- MadLibs/main.py
+++ MadLibs/main.py
@@ -2,18 +2,7 @@
 import json
 import os
 
-class MadLibs:
-    def _init_(self, word_descriptions, template):
-        self.template = templateself.word_descriptions = word_descriptions 
 
-
-def get_words_from_user(word_descriptions):
-    words = []
-    print("Please provide the following words: ")
-    for desc in word_descriptions:
-        user_input = input(desc + " ")
-        words.append(user_input)
-    return words
 
 #Story Building
 # def build_story(template, words):
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
-
-# Add the following import
 from django.http import HttpResponse
 
+
+class MadLibs:
+    def _init_(self, word_descriptions, template):
+        self.template = templateself.word_descriptions = word_descriptions 
+
+
+def get_words_from_user(word_descriptions):
+    words = []
+    print("Please provide the following words: ")
+    for desc in word_descriptions:
+        user_input = input(desc + " ")
+        words.append(user_input)
+    return words
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Welcome to Mad Libs Generator(HomePage)!</h1>')
 
 def about(request):
     return render(request, 'about.html')
+
+def madlibs_index(request):
+    return render(request, 'madlibs/index.html', { 'madlibs' : madlibs } )
