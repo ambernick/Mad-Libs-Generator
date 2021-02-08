dd604fe758ab4b92f1e947aa4556eba1111c8c5e
ambernick <ambernich2@yahoo.com>
2021-02-08 12:25:05 -0500

    render detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('madlibs/', views.madlibs_index, name='index'),
-    path('madlibs/create/', views.MadLibCreate.as_view(), name='madlibs_create'),
+    path('madlibs/<int:madlib_id>/', views.madlibs_detail, name='detail'),
+    path('madlibs/create/', views.MadLibCreate.as_view(), name='madlibs_create')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView
 from .models import MadLib
 
 class MadLibCreate(CreateView):
   model = MadLib
   fields = '__all__'
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
+class MadLibDelete(DeleteView):
+  model = MadLib
+  success_url = '/madlibs/' 
 
 # Define the home view
 def home(request):
@@ -17,3 +24,11 @@ def about(request):
 def madlibs_index(request):
   madlibs = MadLib.objects.all()
   return render(request, 'madlibs/index.html', { 'madlibs': madlibs })
+
+def madlibs_detail(request, madlib_id):
+  madlib = MadLib.objects.get(id=madlib_id)
+  # instantiate FeedingForm to be rendered in the template
+  return render(request, 'madlibs/detail.html', {
+    # pass the madlib and feeding_form as context
+    'madlib': madlib, 
+  })
